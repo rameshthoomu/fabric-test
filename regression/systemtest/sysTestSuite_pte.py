8afcf2aa132773901df802d8f860c6ee02c511b0
Ramesh Babu <rameshbabu.thoomu@gmail.com>
2019-12-19 15:23:28 -0500

    Update sysTestSuite_pte.py

--- regression/systemtest/sysTestSuite_pte.py
+++ regression/systemtest/sysTestSuite_pte.py
@@ -49,12 +49,12 @@ class System_Tests_Kafka_Couchdb_TLS(unittest.TestCase):
 
         # check the counts
         count = subprocess.check_output(
-                "grep \"CONSTANT INVOKE Overall transactions: sent 8000 received 8000\" samplecc_go_2chan_i_pteReport.txt | wc -l",
+                "grep \"CONSTANT INVOKE Overall transactions: sent 400 received 400\" samplecc_go_2chan_i_pteReport.txt | wc -l",
                 cwd=logs_directory, shell=True)
         self.assertEqual(int(count.strip()), 1, msg=invokeFailure)
 
         count = subprocess.check_output(
-                "grep \"CONSTANT QUERY Overall transactions: sent 8000 received 8000 failures 0\" samplecc_go_2chan_q_pteReport.txt | wc -l",
+                "grep \"CONSTANT QUERY Overall transactions: sent 400 received 400 failures 0\" samplecc_go_2chan_q_pteReport.txt | wc -l",
                 cwd=logs_directory, shell=True)
         self.assertEqual(int(count.strip()), 1, msg=queryCountFailure)
 
